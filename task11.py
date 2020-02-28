be95ea04c82178e7e3c4fb37fa7fc1c3399712f2
igorpriadein <iga2456@gmail.com>
2020-02-28 18:30:05 +0600

    Task11 Part2

---
+++ task11.py
@@ -0,0 +1,6 @@
+# Given a string consisting of words separated by spaces. Determine how many
+# words it has. To solve the problem, use the method count.
+
+string = "lol here is a string he  hahah"
+sum = string.split()
+print('your string contains', len(sum), 'words')
